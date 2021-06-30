1295a1e680d1ad8a24641f38873baf52299ec75f
442168402@qq.com <442168402@qq.com>
2021-06-30 20:10:19 +0800

    add netty

---
+++ src/main/java/week02/nio01/netty/HttpHandler.java
@@ -0,0 +1,55 @@
+package week02.nio01.netty;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.*;
+
+import java.io.UnsupportedEncodingException;
+
+import static io.netty.handler.codec.http.HttpHeaderNames.CONNECTION;
+import static io.netty.handler.codec.http.HttpHeaderValues.KEEP_ALIVE;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+public class HttpHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        FullHttpRequest fullHttpRequest = (FullHttpRequest) msg;
+        String uri = fullHttpRequest.uri();
+        if (uri.contains("/test")){
+            handlerTest(fullHttpRequest, ctx, "Hello nio netty");
+        } else {
+            handlerTest(fullHttpRequest, ctx, "hello other");
+        }
+    }
+
+    private void handlerTest(FullHttpRequest fullHttpRequest, ChannelHandlerContext ctx, String body) {
+        FullHttpResponse response = null;
+        String value = body;
+        try {
+            response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(value.getBytes("UTF-8")));
+            response.headers().set("Content-Type", "application/json");
+            response.headers().setInt("Content-Length", response.content().readableBytes());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } finally {
+            if (fullHttpRequest != null){
+                if (!HttpUtil.isKeepAlive(fullHttpRequest)){
+                    ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+                } else {
+                    response.headers().set(CONNECTION, KEEP_ALIVE);
+                    ctx.write(response);
+                }
+                ctx.flush();
+            }
+        }
+    }
+}
